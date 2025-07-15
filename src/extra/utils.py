7b01cb418acf90cbc9e329f6b4f7cb3c3303fdee
shadowik <zahar.mark@bk.ru>
2025-07-15 18:50:21 +0400

    4 lab

--- src/extra/utils.py
+++ src/extra/utils.py
@@ -83,6 +83,49 @@ def split_stratified_into_train_val_test(
     return df_train, df_val, df_test, y_train, y_val, y_test
 
 
+def split_into_train_val_test(
+    df_input,
+    stratify_colname="y",
+    frac_train=0.6,
+    frac_val=0.15,
+    frac_test=0.25,
+    random_state=None,
+) -> Tuple[DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame]:
+    if frac_train + frac_val + frac_test != 1.0:
+        raise ValueError(
+            "fractions %f, %f, %f do not add up to 1.0"
+            % (frac_train, frac_val, frac_test)
+        )
+
+    if stratify_colname not in df_input.columns:
+        raise ValueError("%s is not a column in the dataframe" % (stratify_colname))
+
+    X = df_input  # Contains all columns.
+    y = df_input[
+        [stratify_colname]
+    ]  # Dataframe of just the column on which to stratify.
+
+    # Split original dataframe into train and temp dataframes.
+    df_train, df_temp, y_train, y_temp = train_test_split(
+        X, y, test_size=(1.0 - frac_train), random_state=random_state
+    )
+
+    if frac_val <= 0:
+        assert len(df_input) == len(df_train) + len(df_temp)
+        return df_train, pd.DataFrame(), df_temp, y_train, pd.DataFrame(), y_temp
+
+    # Split the temp dataframe into val and test dataframes.
+    relative_frac_test = frac_test / (frac_val + frac_test)
+    df_val, df_test, y_val, y_test = train_test_split(
+        df_temp,
+        y_temp,
+        test_size=relative_frac_test,
+        random_state=random_state,
+    )
+
+    assert len(df_input) == len(df_train) + len(df_val) + len(df_test)
+    return df_train, df_val, df_test, y_train, y_val, y_test
+
 def run_classification(
     model: Pipeline,
     X_train: DataFrame,
@@ -123,8 +166,8 @@ def run_regression(
     y_test: DataFrame,
 ) -> Dict:
     result = {}
-    y_train_pred = model.predict(X_train.values)
-    y_test_pred = model.predict(X_test.values)
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
 
     result["fitted"] = model
     result["train_preds"] = y_train_pred
